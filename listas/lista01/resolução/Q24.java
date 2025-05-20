a192c6c62791cbaa91cc492ef5059eec7bbb60ff
Anthony Matheus <anthonymnf30@gmail.com>
2025-05-20 13:54:36 -0300

    :sparkles: feat: resolução da questão 24

---
+++ listas/lista01/resolução/Q24.java
@@ -0,0 +1,10 @@
+package listas.lista01.resolução;
+
+public class Q24 {
+  public static void main(String[] args) {
+    int x, y;
+    y = 1;
+    x = 1 + ++y;
+    System.out.println(x--);
+  }
+}
