117308fe37c5812fd99d776f514481f0225f0c47
Anthony Matheus <anthonymnf30@gmail.com>
2025-05-20 13:56:10 -0300

    :sparkles: feat: resolução da questão 29

---
+++ listas/lista01/resolução/Q29.java
@@ -0,0 +1,9 @@
+package listas.lista01.resolução;
+
+public class Q29 {
+  public static void main(String[] args) {
+    int x;
+    x = 10 * 2 % 3 + 5;
+    System.out.println("Valor: " + x++);
+  }
+}
