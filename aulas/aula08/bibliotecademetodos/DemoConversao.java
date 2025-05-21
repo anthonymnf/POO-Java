78ba42c7a1e89a9d7ef5ed0a1be58ec3ca8206ac
Anthony Matheus <anthonymnf30@gmail.com>
2025-05-21 16:13:05 -0300

    :sparkles: feat: Criação de biblioteca de conversão de unidades

---
+++ aulas/aula08/bibliotecademetodos/DemoConversao.java
@@ -0,0 +1,10 @@
+package aulas.aula08.bibliotecademetodos;
+
+public class DemoConversao {
+  public static void main(String[] args) {
+    System.out.println("5 polegadas = " + Unidades.polegadasParaCentimetros(5) + " cm");
+    System.out.println("5 pes = " + Unidades.pesParaCentimetros(5) + " cm");
+    System.out.println("5 milhas = " + Unidades.milhasParaQuilometros(5) + " cm");
+
+  }
+}
---
+++ aulas/aula08/bibliotecademetodos/Unidades.java
@@ -0,0 +1,15 @@
+package aulas.aula08.bibliotecademetodos;
+
+public class Unidades {
+  public static double polegadasParaCentimetros(double pol) {
+    return pol * 2.54;
+  }
+
+  public static double pesParaCentimetros(double pes) {
+    return pes * 30.48;
+  }
+
+  public static double milhasParaQuilometros(double milhas) {
+    return milhas * 1.609;
+  }
+}
