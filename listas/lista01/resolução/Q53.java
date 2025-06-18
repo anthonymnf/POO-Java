dff7582b68c300e83db48301b4bf19235010f08d
Anthony Matheus <anthonymnf30@gmail.com>
2025-06-18 17:34:31 -0300

    :sparkles: feat: Resolução da questão 53 da lista

---
+++ listas/lista01/resolução/Q53.java
@@ -0,0 +1,14 @@
+package listas.lista01.resolução;
+
+public class Q53 {
+  public static void main(String args[]) {
+    int dados[][] = new int[5][];
+    for (int i = 0; i < 5; i++) {
+      dados[i] = new int[5];
+      for (int j = 0; j < 5; j++) {
+        dados[i][j] = i + j;
+        System.out.println(dados[i][j]);
+      }
+    }
+  }
+}
